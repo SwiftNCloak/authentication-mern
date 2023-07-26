8299071f760f42dc7d923835ef9d90a3664db225
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-07-26 15:53:38 +0800

    new improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Register from './components/register'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
+        <Link to={'/'}>Home</Link> |
         <Link to={'/login'}>Login</Link> | 
         <Link to={'/register'}>Register</Link>
       </div>
+      <Routes>
+        <Route path="/register" element = {< Register />} />
+      </Routes>
+      <hr />
       <Register />
-    </BrowserRouter>
+    </Router>
   )
 }
 
--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,7 +1,9 @@
 function Register(){
     return(
         <form action="">
-            <h2>REGISTER</h2>
+            <input type="email" placeholder="email"></input><br />
+            <input type="password" placeholder="password"></input><br />
+            <button type="submit">Submit</button>
         </form>
     )
 }
